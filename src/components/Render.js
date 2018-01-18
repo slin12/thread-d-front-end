27e3ef579a321037b6e81e3da5b58c03089b5d7c
Shirley <s.lin12@gmail.com>
2018-01-18 10:53:25 -0500

    add share buttons to render page

--- src/components/Render.js
+++ src/components/Render.js
@@ -2,11 +2,19 @@ import React from "react";
 import "../css/render.css";
 import * as THREE from "three";
 import React3 from "react-three-renderer";
+import { ShareButtons, generateShareIcon } from "react-share";
 
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
 const OrbitControls = require("three-orbit-controls")(THREE);
+
+//generate icons for sharing
+const TwitterIcon = generateShareIcon("twitter");
+const EmailIcon = generateShareIcon("email");
+const LinkedInIcon = generateShareIcon("linkedin");
+const RedditIcon = generateShareIcon("reddit");
+const url = "http://localhost:3001/render/59f86663-847e-45c8-9bb9-516991cfcdca";
 
 class Render extends React.Component {
   constructor(props, context) {
@@ -87,6 +95,29 @@ class Render extends React.Component {
       };
       return (
         <div id="render">
+          <div id="share-icons">
+            <ShareButtons.TwitterShareButton
+              url={url}
+              title="I designed this t-shirt on thread'd. Check out this awesome 3D model."
+              hashtags={["threadd", "customdesign", "futurefashion"]}
+              children={<TwitterIcon size={32} round={true} />}
+            />
+            <ShareButtons.RedditShareButton
+              url={url}
+              title="I designed a custom t-shirt on thread'd. Check out this awesome 3D model and make you own!"
+              children={<RedditIcon size={32} round={true} />}
+            />
+            <ShareButtons.LinkedinShareButton
+              url={url}
+              title="I designed a custom t-shirt on thread'd. Check out this awesome 3D model and make you own!"
+              description="thread'd was made with React and Rails by Shirley Lin"
+              children={<LinkedInIcon size={32} round={true} />}
+            />
+            <ShareButtons.EmailShareButton
+              url={url}
+              children={<EmailIcon size={32} round={true} />}
+            />
+          </div>
           <React3
             mainCamera="camera"
             width={width}
@@ -125,7 +156,7 @@ class Render extends React.Component {
             <span>
               Click and Drag to move around model. Scroll to zoom in and out.
             </span>
-            <button style={{ visibility: "hidden" }} />
+            <button style={{ display: "hidden" }} />
           </div>
         </div>
       );
