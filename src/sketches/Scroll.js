521a7ca551b3534cb65465ae9893ea8d1471e875
Shirley <s.lin12@gmail.com>
2018-01-25 15:03:27 -0500

    add transparent backgrounds

--- src/sketches/Scroll.js
+++ src/sketches/Scroll.js
@@ -26,6 +26,7 @@ export default function sketch(p) {
         let frame = data[0];
         let blob = AWS.dataURItoBlob(frame.imageData);
         let file = new File([blob], `${uuid()}.jpg`, { type: "image/jpeg" });
+        console.log(file);
         AWS.sendFile(file)
           .then(data => {
             props.createPattern(data.location, props.history);
